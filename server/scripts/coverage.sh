671edcfcaa001531867f8f713cf0e9687785cebb
dzherb <zherbin.dima@yandex.ru>
2025-02-25 01:21:04 +0300

    update scripts

--- server/scripts/coverage.sh
+++ server/scripts/coverage.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 coverage run --source='../' manage.py test
 coverage html
+
+coverage_index_path=$(cd htmlcov && pwd)/index.html
+python -m webbrowser -t file:///$coverage_index_path
